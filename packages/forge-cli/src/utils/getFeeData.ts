ca6346175698ba4a6d879cbfbf4d6f18bd032210
w3stside <david.sato64@gmail.com>
2024-08-27 22:29:56 +0200

    updates to gas fee data logic
    - logic to sanitise fee return data
    - adapt fee data amounts to better get accepted

--- packages/forge-cli/src/utils/getFeeData.ts
+++ packages/forge-cli/src/utils/getFeeData.ts
@@ -4,25 +4,54 @@ import { parseUnits } from '@ethersproject/units'
 import { SupportedNetworks } from '../types/networks'
 import { getGasStationUri } from './getGasStationUri'
 
+interface FeeInfoDetailed {
+  maxFee: number
+  maxPriorityFee: number
+}
+
+const FALLBACK_GWEI = 40000000000
+const RETRY_MULTIPLIER = 12
+const RETRY_BASE = 10
+const MAX_PRIORITY_FEE = 12
+
 export async function getFeeData(network: SupportedNetworks, tryHigherValues = false) {
-  const [multiplier, base] = tryHigherValues ? [12, 10] : [1, 1]
+  const [multiplier, base] = tryHigherValues ? [RETRY_MULTIPLIER, RETRY_BASE] : [1, 1]
   // get max fees from gas station
-  let maxFeePerGas = BigNumber.from(40000000000) // fallback to 40 gwei
-  let maxPriorityFeePerGas = BigNumber.from(40000000000) // fallback to 40 gwei
+  let maxFeePerGas = BigNumber.from(FALLBACK_GWEI) // fallback to 40 gwei
+  let maxPriorityFeePerGas = parseUnits(MAX_PRIORITY_FEE.toString(), 'gwei') // fallback to 40 gwei
+  let gasPrice = maxFeePerGas
   try {
     const gasStationUri = getGasStationUri(network)
     console.log('[Forge-CLI] Gas Station URI:', gasStationUri)
     const res = await (await fetch(gasStationUri)).json()
-    const data = res?.fast ?? res?.data?.fast
-    console.log('[Forge-CLI] Gas Station response:', data)
-    maxFeePerGas = parseUnits(Math.ceil(data.maxFee) + '', 'gwei')
-      .mul(multiplier)
-      .div(base)
-    maxPriorityFeePerGas = parseUnits(Math.ceil(data.maxPriorityFee) + '', 'gwei')
-      .mul(multiplier)
-      .div(base)
+    const data: number | FeeInfoDetailed = res?.fast ?? res?.data?.fast
+    console.log('[Forge-CLI] Gas Station response:', data, typeof data)
+    if (!(data as FeeInfoDetailed)?.maxFee && !data) throw new Error('Missing fee data. Check URI')
+
+    if (typeof data === 'number') {
+      maxFeePerGas = BigNumber.from(data)
+    } else if ('maxFee' in data || 'maxPriorityFee' in data) {
+      // object returns (polygon) return units in human readable format, we need to set to gwei
+      // string returns are already in gwei
+      maxFeePerGas = data?.maxFee ? parseUnits(data.maxFee.toString(), 'gwei') : maxFeePerGas
+      maxPriorityFeePerGas = parseUnits(
+        data?.maxPriorityFee ? data.maxPriorityFee.toString() : MAX_PRIORITY_FEE.toString(),
+        'gwei'
+      )
+    } else throw new Error('Fee info missing. Check URI')
+
+    // set gas price to a percentage higher right away
+    gasPrice = maxFeePerGas.mul(multiplier).div(base)
+    // manually set the maxFee double the recommended to ensure accepted transaction
+    // this is useful for testnets like sepolia that have a lot of variable gas prices
+    maxFeePerGas = gasPrice.mul(2)
+    // sometimes this logic can lead to priority fee being greater than the fee per gas
+    // this changes the priority fee to be half the gas price
+    if (maxPriorityFeePerGas.gte(maxFeePerGas)) {
+      maxPriorityFeePerGas = gasPrice.div(2)
+    }
   } catch (error) {
-    console.error(error)
+    console.error('Error getting fee data!', error)
   }
 
   const feeData = {
@@ -32,6 +61,7 @@ export async function getFeeData(network: SupportedNetworks, tryHigherValues = f
   console.log(`
     Fee Data:
     ---------
+    Current gas price:        ${gasPrice.toString()}
     Max Fee Per Gas:          ${feeData.maxFeePerGas.toString()}
     Max Priority Fee Per Gas: ${feeData.maxPriorityFeePerGas.toString()}
   `)
